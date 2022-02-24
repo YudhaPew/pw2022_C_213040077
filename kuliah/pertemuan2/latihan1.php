e216227df376d24ce2c907006889c21689582b43
YudhaPew <yudhaprasetya678@gmail.com>
2022-02-24 17:10:45 +0700

    Latihan PHP pertemuan 2

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,41 @@
+<?php 
+// Latihan Pertemuan 2 - PHP Dasar
+
+// Variabel
+// Nilai: Angka(integer), Tulisan (string), Boolean (true/false)
+
+// Penulisan box : Nama bebas, tidak boleh spasi
+// Boleh angka, tapi tidak di awal
+
+$nama = "Yudha Prasetya";
+echo $nama;
+echo "<hr>";
+
+// OPERATOR : Aritmatika : + ,- ,* ,/ (kabataku)
+echo (1 + 2) * 3 - 4;
+echo "<br>";
+
+echo 10 % 5;
+
+echo "<hr>";
+// perbandingan : <, >, <=, ==, !=
+echo 2 < 4;
+
+echo "<br>";
+// Identitas
+// ===, !==
+echo 10 === 10;
+
+echo "<hr>";
+// increment / decrement 1 : ++, --
+$x = 10;
+$x++;
+echo $x;
+echo "<hr>";
+
+// concat, penggabungan string ; .
+$nama_depan = "Yudha" ;
+$nama_belakang = "Prasetya" ;
+echo $nama_depan . " " . $nama_belakang ;
+
+?>
